73bd17da252a45cd3e77424b9b16f9eb14696e8f
effintoast <greysonpuckett@gmail.com>
2015-10-07 19:21:03 -0400

    Button changes

--- PyPiAuto.py
+++ PyPiAuto.py
@@ -1,4 +1,4 @@
-import pygame, sys, Buttons, time
+import pygame, sys, Buttons, time, pygame.gfxdraw
 from pygame.locals import *
 
 pygame.init()
@@ -12,6 +12,9 @@ S_HEIGHT = 480
 MENU_COLOR = (87,87,87)
 MENU_COLOR_ACTIVE = (255,255,255)
 BUTTON_BG_COLOR = (0,0,0)
+BUTTON_ON_COLOR = (71,181,32)
+BUTTON_OFF_COLOR = (95,95,95)
+
 
 #setup canvas
 windowCanvas = pygame.display.set_mode((S_WIDTH, S_HEIGHT), 0, 32)
@@ -25,14 +28,15 @@ clock = pygame.time.Clock()
 menuOptions = {'lights': 'LIGHTS', 'winch': 'WINCH', 'settings': 'SETTINGS'};
 toggleButtons = {
 	'lights': [
-		{'title': 'TOP LIGHTS', 'toggleIO': 21},
-		{'title': 'REAR LIGHTS', 'toggleIO': 22},
-		{'title': 'BUMPER LIGHTS', 'toggleIO': 24},
-		{'title': 'CABIN LIGHTS', 'toggleIO': 26}
+		{'title': 'TOP LIGHTS', 'toggleIO': 21, 'status': 0},
+		{'title': 'REAR LIGHTS', 'toggleIO': 22, 'status': 0},
+		{'title': 'BUMPER LIGHTS', 'toggleIO': 24, 'status': 0},
+		{'title': 'CABIN LIGHTS', 'toggleIO': 26, 'status': 0},
+		{'title': 'TOGGLE ALL', 'toggleIO': 'sys', 'func': 'toggleAll'}
 		],
 	'winch': [
-		{'title': 'WINCH OUT', 'toggleIO': 27},
-		{'title': 'WINCH IN', 'toggleIO': 28}
+		{'title': 'WINCH OUT', 'toggleIO': 27, 'status': 0},
+		{'title': 'WINCH IN', 'toggleIO': 28, 'status': 0}
 		]
 	}
 
@@ -41,6 +45,24 @@ currentPane = 'lights'
 
 for key, title in menuOptions.iteritems():
 	menuOptions[key] = [ menuOptions[key],Buttons.MenuOption()]
+
+def changePinState(pin, status):
+
+
+def toggleAll(buttonKey, button):
+	if 'toggleStatus' in button:
+		if button['toggleStatus'] == 1:
+			button['toggleStatus'] = 0
+		else:
+			button['toggleStatus'] = 1
+	else:
+		button['toggleStatus'] = 1
+
+
+	if currentPane in toggleButtons:
+		for key,data in enumerate(toggleButtons[currentPane]):
+			if 'status' in toggleButtons[currentPane][key]:
+				toggleButtons[currentPane][key]['status'] = button['toggleStatus']
 
 def drawToggleButtons(targetButton=None):
 	topOffset = 25
@@ -59,7 +81,7 @@ def drawToggleButtons(targetButton=None):
 				tmpBGColor = (255,255,255)
 				tmpTxtColor = LEFT_PANE_BG_COLOR
 			toggleButtons[key][keyb]['button'] = Buttons.ToggleOption()
-			toggleButtons[key][keyb]['button'].create_button(windowCanvas, tmpBGColor, (leftOffset+tempLeftOffset), topOffset, toggleButtons[key][keyb]['title'], tmpTxtColor)
+			toggleButtons[key][keyb]['button'].create_button(windowCanvas, tmpBGColor, (leftOffset+tempLeftOffset), topOffset, toggleButtons[key][keyb]['title'], tmpTxtColor, BUTTON_ON_COLOR, BUTTON_OFF_COLOR, toggleButtons[key][keyb])
 			#pygame.draw.rect(windowCanvas, (0,0,0), (leftOffset+tempLeftOffset,topOffset, 155, 110))
 			
 			i = i + 1;
@@ -77,6 +99,7 @@ def setupRightPane():
 #clear canvas and redraw menu
 def clearRightPane():
 	windowCanvas.fill(BG_COLOR)
+	pygame.draw.rect(windowCanvas, (44,44,44), (0, 0, 226, 481))
 	pygame.draw.rect(windowCanvas, LEFT_PANE_BG_COLOR, (0, 0, 225, 480))
 	updateMainMenu()
 	pygame.display.flip()
@@ -109,9 +132,16 @@ while True:
 			if currentPane in toggleButtons:
 				for key,data in enumerate(toggleButtons[currentPane]):
 					if toggleButtons[currentPane][key]['button'].pressed(pygame.mouse.get_pos()):
-						print toggleButtons[currentPane][key]['title']+'- pin '+str(toggleButtons[currentPane][key]['toggleIO']);
-						#redraw to show button was clicked
-						drawToggleButtons(key)
-						time.sleep(.2)
+						if toggleButtons[currentPane][key]['toggleIO'] == 'sys':
+							globals()[toggleButtons[currentPane][key]['func']](key,toggleButtons[currentPane][key])
+						if 'status' in toggleButtons[currentPane][key]:
+							if toggleButtons[currentPane][key]['status'] == 0:
+								toggleButtons[currentPane][key]['status'] = 1
+							else:
+								toggleButtons[currentPane][key]['status'] = 0
+						else:
+							#redraw to show button was clicked
+							drawToggleButtons(key)
+							time.sleep(.2)
 						drawToggleButtons()
 	clock.tick(15)
